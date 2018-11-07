bc281ff86c4aed7b02859afc841b1a2322e958bd
kevin joya <kevin.joya@outlook.com>
2018-11-07 20:50:48 +0100

    Yelp push

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: "controller#action"
+  namespace :admin do
+    resources :restaurants, only: [:index, :update, :delete]
+  end
+  resources :restaurants do
+    #get "restaurants/:restaurant_id/reviews", to: "reviews#index"
+    resources :reviews, only: [:index, :new, :create]
+  end
+  resources :reviews, only: [:show, :edit, :update, :destroy]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         'Dishoom',
+    address:      '7 Boundary St, London E2 7JE',
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  },
+  {
+    name:         'Pizza East',
+    address:      'Bordeaux',
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  },
+  {
+    name:         'Eat salad',
+    address:      'St Catherine',
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  },
+  {
+    name:         'Mcdo',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  },
+  {
+    name:         'Kokomo',
+    address:      '56A Shoreditch High St, London E1 6PQ',
+    category: ['chinese', 'italian', 'japanese', 'french', 'belgian'].sample
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
